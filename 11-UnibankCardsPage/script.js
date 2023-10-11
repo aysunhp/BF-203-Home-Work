39c1c033c074ea60a0d25727ac8c4efa542b26fe
aysunhp <145673599+aysunhp@users.noreply.github.com>
2023-10-11 13:26:11 +0400

    unibank card page added

---
+++ 11-UnibankCardsPage/script.js
@@ -0,0 +1,103 @@
+// var's initialization,declaration, reassign
+
+var num1 = 7;
+console.log(num1);
+num1 = 3;
+console.log(num1);
+num1 = 5;
+num1 = 9;
+console.log(num1);
+num1 = 5;
+var num1 = 19;
+var word1 = "hello";
+console.log(num1);
+console.log(word1);
+
+console.log(typeof num1);
+console.log(typeof word1);
+console.log(num1 + word1);
+console.log(word1 + num1);
+
+var word1 = "salam";
+function greet1() {
+  console.log(word1);
+}
+greet1();
+
+function greet2() {
+  var word2 = "hi";
+  console.log(word2);
+}
+
+greet2();
+
+// let's initialization,declaration, reassign
+
+let num3 = 7;
+console.log(num3);
+num3 = 3;
+console.log(num3);
+num3 = 5;
+num3 = 9;
+console.log(num3);
+num3 = 5;
+// let num3 = 19;
+
+let word5 = "hello";
+console.log(word5);
+
+console.log(typeof num3);
+console.log(typeof word5);
+console.log(num3 + word5);
+console.log(word5 + num3);
+
+// let word6 = "salam";
+// function greet1() {
+//   console.log(word6);
+// }
+// greet1();
+
+function greet2() {
+  let word4 = "hi";
+  console.log(word4);
+}
+
+greet2();
+
+// function greet3() {
+//   let word7 = "how are you";
+// }
+// console.log(word7);
+// greet3();
+
+// let's initialization,declaration, reassign
+
+const eded = 21;
+console.log(eded);
+
+// eded = 22;
+// console.log(eded);
+const soz = "salam";
+console.log(soz);
+// soz = "aysun";
+
+console.log(eded - soz);
+console.log(eded * soz);
+console.log(soz + eded);
+
+const soz1 = "hi";
+console.log(soz + eded + soz1);
+console.log(soz + eded * soz1);
+console.log(soz * eded + soz1);
+console.log(soz * eded * soz1);
+console.log(soz + eded - soz1);
+console.log(soz - eded + soz1);
+
+const soz2 = "aysun";
+console.log(soz2);
+console.log(3 + 4 + soz2);
+console.log(3 + 4 + soz2 + 4);
+console.log(3 + 4 + soz2 + 4 + 5);
+console.log(3 * 4 * soz2 + 4);
+console.log(typeof (3 * 4 * soz2 + 4));
+console.log(typeof number);
